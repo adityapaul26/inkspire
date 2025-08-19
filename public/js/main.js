7f57256d5f55fb49a12a182254bdb82036c00fc8
Aditya <adityapaul2603@gmail.com>
2025-08-19 22:22:58 +0530

    animation_add

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,62 @@
-// Future JS logic can go here
-console.log('My Blog frontend is ready');
+let words=['INKSPIRE','Insights','Stories','Ideas','Trends','Perspectives']
+
+function delay1(){
+    return new Promise((resolve, reject) => {
+        setInterval(() => {
+            resolve(1)
+        }, 100);
+    })
+}
+
+function delay2(){
+    return new Promise((resolve, reject) => {
+        setInterval(() => {
+            resolve(1)
+        }, 200);
+    })  
+}
+
+async function main(){
+    let item = document.querySelector(".logo");
+    while(true){
+        for(const i of words){
+            //console.log(i);
+            if(i==='INKSPIRE') {
+                item.style.color = "#d99100";
+                // item.style.border = "4.8px solid #d99100";
+                document.getElementById("header").style.border = "4.8px solid #d99100";
+            }else{
+                item.style.color = "#e5be89";
+                // item.style.border = "4.8px solid #d99100";
+                document.getElementById("header").style.border = "4.8px solid #e5be89";
+            }
+            
+            // if(i==='INKSPIRE') item.classList.add("gold");
+            // if(i==='Insights') item.classList.remove("gold");
+            for(const j of i){
+                // console.log(j)
+                item.innerHTML=  item.innerHTML + j
+                await delay1()
+            }
+            await delay2();
+            let del=item.innerHTML
+    
+            while(del.length > 0){
+                del = del.slice(0,-1)
+                item.innerHTML=del
+                await delay1()
+            }
+            await delay2();
+        }
+    }
+}
+
+main()
+
+
+
+
+
+
+
+
